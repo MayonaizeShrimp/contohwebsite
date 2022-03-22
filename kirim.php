2ac323294d2f1c55ba7fb2fa28038ac15fe88900
MayonaizeShrimp <85448449+MayonaizeShrimp@users.noreply.github.com>
2022-03-22 11:48:13 +0700

    contoh kirim email
    Jangan dirun kodingannya. Contoh doang pasti ngebug

--- kirim.php
+++ kirim.php
@@ -1,9 +1,26 @@
 <?php
 	include 'LoginData.php';
+	include 'kirimemail.php';
 
-	$loginData = new LoginData();
+	$emailHandler = new EmailHandler();
+
+	$emailHandler->name 	= $_POST['name'];
+	$emailHandler->email 	= $_POST['email'];
+	$emailHandler->phone 	= $_POST['phone'];
+
+
+	$emailHandler->sendEmail();
+	
+	$emailHandler->sendWhatsApp();
+
+
+
+
+
+
+	/*$loginData = new LoginData();
 	$loginData->email = $_POST['email'];
 	$loginData->password = $_POST['password'];
 
-	$loginData->verify();
+	$loginData->verify();*/
 ?>
